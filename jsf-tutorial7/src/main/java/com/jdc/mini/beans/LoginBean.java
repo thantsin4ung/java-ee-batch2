aa907abc8484bcb48085abdf30d4698fef861ced
minlwin <minlwin@192.168.1.3>
2017-11-19 13:06:16 +0630

    Add Database

--- jsf-tutorial7/src/main/java/com/jdc/mini/beans/LoginBean.java
+++ jsf-tutorial7/src/main/java/com/jdc/mini/beans/LoginBean.java
@@ -1,16 +1,19 @@
 package com.jdc.mini.beans;
 
+import javax.enterprise.inject.Model;
+
+@Model
 public class LoginBean {
 
 	private String loginId;
 	private String password;
 
 	public String login() {
-		return "/view/balance-report";
+		return "/view/balance-report?faces-redirect=true";
 	}
 
 	public String logout() {
-		return "/login";
+		return "/login?faces-redirect=true";
 	}
 
 	public String getLoginId() {
